758cc540f670dfa75de977c98530cfc1a5a8d811
oaluna <oscarluna.webdev@gmail.com>
2022-04-29 14:52:46 -0700

    added new footer component jsx

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./Footer.css";
-// For styling any nested component, Overlay use the styled method.
-// This method works perfectly on all of your any third-party component, as long as they attach the passed className prop to a DOM element.
-// Here an example: https://gist.github.com/Miniplop/80b042d1b44293329ef7332fd47b770c
+import OscarArmandoLunaLogo from "../../assets/OscarArmandoLunaLogo.svg";
 
-// Nested component imports
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import WorkIcon from "@mui/icons-material/Work";
@@ -13,77 +10,72 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer-menu">
-        <div className="home-three">
-          <div className="frame-3537"></div>
-          <div className="frame-3537">
-            <HomeIcon />
-            <div className="home-two">Home</div>
-          </div>
-        </div>
-        <div className="home-three">
-          <div className="frame-3537"></div>
-          <div className="frame-3537">
-            <PersonIcon />
-            <div className="home-two">About me</div>
-          </div>
-        </div>
-        <div className="home-three">
-          <div className="frame-3537"></div>
-          <div className="frame-3537">
-            <WorkIcon />
-            <div className="home-two">Work</div>
-          </div>
-        </div>
-        <div className="home-three">
-          <div className="frame-3537"></div>
-          <div className="frame-3537">
-            <PhoneIcon />
-            <div className="home-two">Contact</div>
-          </div>
-        </div>
-        <div className="home-three">
-          <div className="frame-3537"></div>
-          <div className="frame-3537">
-            <SearchIcon />
-            <div className="home-two">Search</div>
-          </div>
+   <footer className="mainfooter" role="contentinfo">
+  <div className="footer-middle">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3 col-sm-6">
+       
+        <div className="footer-pad">
+          <h4>Heading 1</h4>
+          <ul className="list-unstyled">
+            <li><a href="#"></a></li>
+            <li><a href="#">Payment Center</a></li>
+            <li><a href="#">Contact Directory</a></li>
+            <li><a href="#">Forms</a></li>
+            <li><a href="#">News and Updates</a></li>
+            <li><a href="#">FAQs</a></li>
+          </ul>
         </div>
       </div>
-      <div className="social-media">
-        <div className="icons">
-          <img
-            alt=""
-            src="https://static.overlay-tech.com/assets/e5ec421e-6f8e-4d82-a080-bbe112013777.png"
-          />
-          <img
-            alt=""
-            src="https://static.overlay-tech.com/assets/674ab348-621b-4d40-b2eb-b50886ee2dc0.png"
-          />
-          <img
-            alt=""
-            src="https://static.overlay-tech.com/assets/1ec4f2b4-4da9-4391-ad13-97d398bbf1ed.png"
-          />
-          <img
-            alt=""
-            src="https://static.overlay-tech.com/assets/5719a54f-2cf4-45b6-9161-7dad86b3fab0.png"
-          />
-
-          <img
-            alt=""
-            src="https://static.overlay-tech.com/assets/f72a9a76-a351-4c94-8f9f-5d4d7d0e7800.svg"
-          />
-
-          <img
-            alt=""
-            src="https://static.overlay-tech.com/assets/7c4cc6ff-d232-4f80-ac20-f13e7bf29cfd.svg"
-          />
+      <div className="col-md-3 col-sm-6">
+        
+        <div className="footer-pad">
+          <h4>Heading 2</h4>
+          <ul className="list-unstyled">
+            <li><a href="#">Website Tutorial</a></li>
+            <li><a href="#">Accessibility</a></li>
+            <li><a href="#">Disclaimer</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Webmaster</a></li>
+          </ul>
         </div>
       </div>
-      <span className="separator" />
-      <div className="footer-terms">Terms of Service - Privacy Policy</div>
-    </footer>
+      <div className="col-md-3 col-sm-6">
+       
+        <div className="footer-pad">
+          <h4>Heading 3</h4>
+          <ul className="list-unstyled">
+            <li><a href="#">Parks and Recreation</a></li>
+            <li><a href="#">Public Works</a></li>
+            <li><a href="#">Police Department</a></li>
+            <li><a href="#">Fire</a></li>
+            <li><a href="#">Mayor and City Council</a></li>
+            <li>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    	<div className="col-md-3">
+    		<h4>Follow Us</h4>
+            <ul className="social-network social-circle">
+             <li><a href="#" className="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
+             <li><a href="#" className="icoLinkedin" title="Linkedin"><i className="fa fa-linkedin"></i></a></li>
+            </ul>				
+		</div>
+    </div>
+	<div className="row">
+		<div className="col-md-12 copy">
+			<p className="text-center">&copy; Copyright 2018 - Company Name.  All rights reserved.</p>
+		</div>
+	</div>
+
+
+  </div>
+  </div>
+</footer>
   );
 };
 
